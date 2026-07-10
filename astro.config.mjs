d68d96506dc152326ef9a382a75f510c81e1ba29
mvgn91 <jazzfatale@gmail.com>
2026-07-09 20:18:52 -0600

    fix-site-url-astro-config-cloudflare-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mvgnlabs.vercel.app',
+  site: 'https://mvgn-digital-support-center.pages.dev',
   srcDir: './src',
   publicDir: './public',
   output: 'static',
